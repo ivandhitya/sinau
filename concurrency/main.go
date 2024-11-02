71be576dedea36f4dbda0efa5e1b495870634270
ivandhitya <ivandhitya@gmail.com>
2024-11-02 22:10:23 +0700

    docs: ✏️ add comment

--- concurrency/main.go
+++ concurrency/main.go
@@ -13,14 +13,16 @@ func process(name string, duration time.Duration) {
 }
 
 func main() {
+	// program hanya berjalan dengan menggunakan 1 processor
 	runtime.GOMAXPROCS(1)
 
+	// tugas pertama
 	go func() {
-
 		process("1A", 2*time.Second)
 		process("1B", 1*time.Second)
 	}()
 
+	// tugas kedua
 	go func() {
 		process("2A", 1*time.Second)
 		process("2B", 3*time.Second)
