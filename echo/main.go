7e1008c1cf8c4e6261a54706ae606b764c0bfa1c
ivandhitya <ivandhitya@gmail.com>
2024-11-09 05:39:58 +0700

    feat: 🎸 Echo API & Swagger Doc

--- echo/main.go
+++ echo/main.go
@@ -4,7 +4,10 @@ import (
 	"net/http"
 	"strconv"
 
+	_ "github.com/ivandhitya/sinau/echo/docs"
+
 	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
 // Struct untuk representasi data User
@@ -20,12 +23,23 @@ var users = []User{
 	{ID: 2, Name: "Farel", Age: 37},
 }
 
-// Handler untuk mendapatkan semua user
+// @Summary Mendapatkan daftar pengguna
+// @Description Mendapatkan seluruh pengguna
+// @Tags         users
+// @Success 200 {object} map[string]interface{}
+// @Router /users [get]
 func getUsers(c echo.Context) error {
 	return c.JSON(http.StatusOK, users)
 }
 
-// Handler untuk mendapatkan user berdasarkan ID
+// @Summary      Get user by ID
+// @Description  Get details for a specific user by their ID
+// @Tags         users
+// @Param        id   path      int     true  "User ID"
+// @Success      200  {object}  User
+// @Failure      400  {object}  string
+// @Failure      404  {object}  string
+// @Router       /users/{id} [get]
 func getUser(c echo.Context) error {
 	id := c.Param("id")
 	idTemp, err := strconv.Atoi(id)
@@ -41,7 +55,16 @@ func getUser(c echo.Context) error {
 	return c.JSON(http.StatusNotFound, "User not found")
 }
 
-// Handler untuk membuat atau menambahkan user baru
+// createUser godoc
+// @Summary      Create a new user
+// @Description  This endpoint allows creating a new user with the provided details
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        user  body      User  true  "User Data"
+// @Success      201   {object}  User
+// @Failure      400   {object}  string  "Bad Request"
+// @Router       /users [post]
 func createUser(c echo.Context) error {
 	user := new(User)
 	if err := c.Bind(user); err != nil {
@@ -51,6 +74,9 @@ func createUser(c echo.Context) error {
 	return c.JSON(http.StatusCreated, user)
 }
 
+// @title Sample API
+// @version 1.0
+// @description Ini adalah dcontoh dokumentasi API
 func main() {
 	e := echo.New()
 
@@ -58,6 +84,11 @@ func main() {
 	e.GET("/users", getUsers)
 	e.GET("/users/:id", getUser)
 	e.POST("/users", createUser)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "hallo aku server echo")
+	})
 
 	// Start server
 	e.Logger.Fatal(e.Start(":7777"))
